b6f8763766c6760262208915131fcbed424be278
Richard Palacios G <apalaciosg91@gmail.com>
2018-09-08 22:43:48 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rapg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'live-kc';
+}
